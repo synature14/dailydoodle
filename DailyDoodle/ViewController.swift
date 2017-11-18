9fd730bd385645d8c269bec00d96dcf43d7b1f63
Ahn DeockHwan <ahn.thekan@navercorp.com>
2017-11-18 22:00:42 +0900

    Initial Commit

---
+++ DailyDoodle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyDoodle
+//
+//  Created by 안덕환 on 2017. 11. 18..
+//  Copyright © 2017년 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
